3b1285ab8de07ba9ec509fc2ed989053b0160a9b
Steve Baer <steve@mcneel.com>
2015-05-20 15:46:02 -0700

    update config scripts and move pages to appropriate locations

--- doku_setup.sh
+++ doku_setup.sh
@@ -8,32 +8,32 @@
 
 
 #update apt-get
-apt-get update
-apt-get install lighttpd -y
-apt-get install php5-cgi -y
-apt-get install git -y
+sudo apt-get update
+sudo apt-get install lighttpd -y
+sudo apt-get install php5-cgi -y
+sudo apt-get install git -y
 
 #download latest dokuwiki tarball
 wget http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
 #unzip tarball
 tar xvfv dokuwiki-stable.tgz
 #move/rename contents to /var/www/dokuwiki
-mv dokuwiki-2014-09-29d/ /var/www/dokuwiki/
+sudo mv dokuwiki-2014-09-29d/ /var/www/dokuwiki/
 
 #change owner to www-data
-chown -R www-data:www-data /var/www/dokuwiki/
+sudo chown -R www-data:www-data /var/www/dokuwiki/
 
 #configure lightttpd for php
-lighttpd-enable-mod fastcgi-php
+sudo lighttpd-enable-mod fastcgi-php
 
-git clone https://github.com/mcneel/wikidata.git /var/dokuwiki/wikidata
-chown -R www-data:www-data /var/www/dokuwiki/wikidata
+sudo git clone https://github.com/mcneel/wikidata.git /var/www/dokuwiki/wikidata
+sudo chown -R www-data:www-data /var/www/dokuwiki/wikidata
 
 # copy configuration files to appropriate locations
-cp /var/www/dokuwiki/wikidata/conf/lighttpd/lighttpd.conf /etc/lighttpd.conf
+sudo cp /var/www/dokuwiki/wikidata/conf/lighttpd/lighttpd.conf /etc/lighttpd.conf
 
-cp /var/www/dokuwiki/wikidata/conf/dokuwiki/conf/* /var/www/dokuwiki/conf/
-cp /var/www/dokuwiki/wikidata/conf/dokuwiki/lib/plugins/* /var/www/dokuwiki/lib/plugins/
-cp /var/www/dokuwiki/wikidata/conf/dokuwiki/lib/tpl/* /var/www/dokuwiki/lib/tpl/
+sudo cp /var/www/dokuwiki/wikidata/conf/dokuwiki/conf/* /var/www/dokuwiki/conf/
+sudo cp -R /var/www/dokuwiki/wikidata/conf/dokuwiki/lib/plugins/* /var/www/dokuwiki/lib/plugins/
+sudo cp -R /var/www/dokuwiki/wikidata/conf/dokuwiki/lib/tpl/* /var/www/dokuwiki/lib/tpl/
 #restart lightttpd
-/etc/init.d/lighttpd force-reload
+sudo /etc/init.d/lighttpd force-reload
