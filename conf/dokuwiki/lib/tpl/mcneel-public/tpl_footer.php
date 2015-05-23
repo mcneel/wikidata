0abb63f15d7b8365b233ac647f6d17c05294ff28
Steve Baer <steve@mcneel.com>
2015-05-22 17:00:54 -0700

    add google analytics to template

--- conf/dokuwiki/lib/tpl/mcneel-public/tpl_footer.php
+++ conf/dokuwiki/lib/tpl/mcneel-public/tpl_footer.php
@@ -29,5 +29,16 @@ if (!defined('DOKU_INC')) die();
     </div>
 </div></div><!-- /footer -->
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-512742-6', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <?php
 tpl_includeFile('footer.html');
