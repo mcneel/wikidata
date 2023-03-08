88c15d3a2ac34c4a954818e21d5bb1ccbae023a7
Scott Davidson <scottd@mcneel.com>
2023-03-08 12:41:31 -0800

    working to get GA4 tag to footer

--- conf/dokuwiki/lib/tpl/mcneel-public/tpl_footer.php
+++ conf/dokuwiki/lib/tpl/mcneel-public/tpl_footer.php
@@ -41,15 +41,5 @@ if (!defined('DOKU_INC')) die();
 
 </script>
 
-<!-- Google tag GA4 --> 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EQ9K187465"></script> 
-<script> 
-    window.dataLayer = window.dataLayer || []; 
-    function gtag(){dataLayer.push(arguments);} 
-    gtag('js', new Date()); 
-    
-    gtag('config', 'G-EQ9K187465'); 
-</script>
-
 <?php
 tpl_includeFile('footer.html');
