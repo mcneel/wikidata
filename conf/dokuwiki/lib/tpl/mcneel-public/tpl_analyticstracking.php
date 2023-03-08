7e97aca343e485a61cab0a3a22d6a2aa93e25649
Scott Davidson <scottd@mcneel.com>
2023-03-08 13:09:45 -0800

    adding PHP GA4 tracking

---
+++ conf/dokuwiki/lib/tpl/mcneel-public/tpl_analyticstracking.php
@@ -0,0 +1,9 @@
+<!-- Google tag GA4 --> 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EQ9K187465"></script> 
+<script> 
+    window.dataLayer = window.dataLayer || []; 
+    function gtag(){dataLayer.push(arguments);} 
+    gtag('js', new Date()); 
+    
+    gtag('config', 'G-EQ9K187465'); 
+</script>
