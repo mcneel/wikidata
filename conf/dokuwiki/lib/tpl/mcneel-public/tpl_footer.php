e01ff7fd2fd85e99e1d72adb379d8124933fa489
Scott Davidson <scottd@mcneel.com>
2023-03-08 11:51:32 -0800

    Updated Commentsfor GA4 tag

--- conf/dokuwiki/lib/tpl/mcneel-public/tpl_footer.php
+++ conf/dokuwiki/lib/tpl/mcneel-public/tpl_footer.php
@@ -41,10 +41,14 @@ if (!defined('DOKU_INC')) die();
 
 </script>
 
-<!-- Google tag GA4 (gtag.js) --> 
+<!-- Google tag GA4 --> 
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-EQ9K187465"></script> 
 <script> 
-    window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-EQ9K187465'); 
+    window.dataLayer = window.dataLayer || []; 
+    function gtag(){dataLayer.push(arguments);} 
+    gtag('js', new Date()); 
+    
+    gtag('config', 'G-EQ9K187465'); 
 </script>
 
 <?php
